055829e9df6fc2ac2cbf717dce3b8f728ce2426b
Le0nX <cloudmindone@gmail.com>
2020-02-01 11:59:32 +0300

    init: init framework

---
+++ FeedModule/FeedItem.swift
@@ -0,0 +1,9 @@
+//
+//  FeedItem.swift
+//  FeedModule
+//
+//  Created by Denis Nefedov on 01.02.2020.
+//  Copyright © 2020 X. All rights reserved.
+//
+
+import Foundation
---
+++ FeedModule/FeedModule/FeedModule.h
@@ -0,0 +1,19 @@
+//
+//  FeedModule.h
+//  FeedModule
+//
+//  Created by Denis Nefedov on 01.02.2020.
+//  Copyright © 2020 X. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FeedModule.
+FOUNDATION_EXPORT double FeedModuleVersionNumber;
+
+//! Project version string for FeedModule.
+FOUNDATION_EXPORT const unsigned char FeedModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FeedModule/PublicHeader.h>
+
+
