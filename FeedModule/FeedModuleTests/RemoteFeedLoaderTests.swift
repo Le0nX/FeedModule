fec7e9dda74431e8535462309226c2e146b73ef9
Le0nX <cloudmindone@gmail.com>
2020-02-02 16:55:24 +0300

    success request from load with Singleton HTTPClient

--- FeedModule/FeedModuleTests/RemoteFeedLoaderTests.swift
+++ FeedModule/FeedModuleTests/RemoteFeedLoaderTests.swift
@@ -11,21 +11,34 @@ import XCTest
 
 class RemoteFeedLoader {
     func load() {
-        
+        HTTPClient.shared.requestedURL = URL(string: "http://example.com")
     }
 }
 
 class HTTPClient {
+    static let shared = HTTPClient()
+    
+    private init() {}
+    
     var requestedURL: URL?
 }
 
 class RemoteFeedLoaderTests: XCTestCase {
     func test_init_doesnotRequestDataFromURL() {
-        let client = HTTPClient()
+        let client = HTTPClient.shared
         _ = RemoteFeedLoader()
         
         XCTAssertNil(client.requestedURL)
         
+    }
+    
+    func test_load_requestDataFromURL() {
+        let client = HTTPClient.shared
+        
+        let sut = RemoteFeedLoader()
+        sut.load()
+        
+        XCTAssertNotNil(client.requestedURL)
     }
     
 }
