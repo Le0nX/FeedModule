7c251e5c1489dc221c2defcbd2f0277e4d6a8972
Le0nX <cloudmindone@gmail.com>
2020-03-02 13:09:04 +0300

    fix: async bug test

--- FeedModule/FeedModuleTests/RemoteFeedLoaderTests.swift
+++ FeedModule/FeedModuleTests/RemoteFeedLoaderTests.swift
@@ -97,6 +97,20 @@ class RemoteFeedLoaderTests: XCTestCase {
                 
     }
     
+    func test_load_doesNotDeliverResultAfterSUThasBeenDeallocated() {
+        let url = URL(string: "http://example.com")!
+        let client = HTTPClientSpy()
+        var sut: RemoteFeedLoader? = RemoteFeedLoader(client: client, url: url)
+        
+        var capturedResults = [RemoteFeedLoader.Result]()
+        sut?.load() { capturedResults.append($0) }
+        
+        sut = nil
+        client.complete(withStatusCode: 200, data: makeItemsJSON([]))
+        
+        XCTAssertTrue(capturedResults.isEmpty)
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(url: URL = URL(string: "http://example.com")!, file: StaticString = #file, line: UInt = #line) -> (HTTPClientSpy, RemoteFeedLoader) {
